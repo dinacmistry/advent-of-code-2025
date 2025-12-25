f637283df9c378ad70c7d3c8d6aa774ca466f9e3
Dina Mistry <dina.c.mistry@gmail.com>
2025-12-24 21:46:54 -0500

    initial code and input needed

---
+++ day-1-dial.py
@@ -0,0 +1,72 @@
+"Day 1: Dial"
+
+
+
+def parse_input(filename):
+    with open(filename, 'r') as f:
+        contents = f.read()
+
+    contents = contents.split("\n")
+    empty = ['']
+    contents = [i for i in contents if i not in empty]
+
+    return contents
+
+
+filename = "day-1-dial-input.txt"
+contents = parse_input(filename)
+# for line in contents:
+#     print(line)
+print(contents)
+print(len(contents))
+
+
+# lines = contents.split("\n")
+# print(lines)
+# print(len(lines))
+
+def get_password(instructions, starting_point):
+    y = starting_point
+    count_of_zeros = 0
+    for i in instructions:
+        direction = i[0]
+        step = i[1:]
+        if direction == "L":
+            y = y - int(step)
+
+        else:
+            y = y + int(step)
+
+        y = y % 100
+
+        if y == 0:
+            count_of_zeros += 1
+
+    return y, count_of_zeros
+
+
+if __name__ == '__main__':
+
+    starting_point = 50
+
+    test_instructions = [
+    "L68", 
+    "L30",
+    "R48",
+    "L5",
+    "R60",
+    "L55",
+    "L1",
+    "L99",
+    "R14",
+    "L82",
+    ]
+
+    expected_y = 32
+    expected_password = 3
+    test_y, test_password = get_password(test_instructions, starting_point)
+    print(test_y, test_password)
+
+    assert test_y == expected_y, "dial point wrong"
+    assert test_password == expected_password, "password wrong"
+
