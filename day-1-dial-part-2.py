4b42ba8648d2c7dc67c427306ac99b99d4925c18
Dina Mistry <dina.c.mistry@gmail.com>
2025-12-25 21:11:25 -0500

    finish day 1 puzzle 2 clean up

--- day-1-dial-part-2.py
+++ day-1-dial-part-2.py
@@ -1,6 +1,6 @@
 "Day 1: Dial part 2"
 import numpy as np
-
+import timeit
 
 def parse_input(filename):
     with open(filename, 'r') as f:
@@ -16,8 +16,7 @@ def get_password_0x434C49434B(instructions, starting_point):
     y = starting_point
     count_of_zeros = 0
     for n, i in enumerate(instructions):
-        # print("old", n, i, y)
-        print()
+
         direction = i[0]
         step = i[1:]
         old_y = y
@@ -27,22 +26,20 @@ def get_password_0x434C49434B(instructions, starting_point):
         else:
             y = y + int(step)
 
-        print("new", n + 1, i, y, old_y)
         range_min = min(y, old_y)
         range_max = max(y, old_y)
         clicks_at_x = [x for x in range(range_min, range_max) if x % 100 == 0]
-        print("clicks_at_x", clicks_at_x)
         number_of_clicks_per_step = len(clicks_at_x)
 
-        y = y % 100
-        print("final", n + 1, i, y, old_y, range_min, range_max, clicks_at_x)
+        if y != old_y:
+            if y % 100 == 0:
+                count_of_zeros += 1
+            if old_y % 100 == 0:
+                count_of_zeros += number_of_clicks_per_step - 1
+            else:
+                count_of_zeros += number_of_clicks_per_step
 
-        if y == 0:
-            count_of_zeros += 1
-            print("adding 1", count_of_zeros)
-        else:
-            count_of_zeros += number_of_clicks_per_step
-            print("adding", number_of_clicks_per_step, count_of_zeros)
+        y = y % 100
 
     return y, count_of_zeros
 
@@ -51,7 +48,6 @@ def get_new_password(instructions, starting_point):
     count_of_zeros = 0
 
     for n, i in enumerate(instructions):
-        print()
         direction = i[0]
         step = i[1:]
         old_y = y
@@ -62,6 +58,7 @@ def get_new_password(instructions, starting_point):
 
         range_min = min(y, old_y)
         range_max = max(y, old_y)
+
         # find the nearest multiple of 100 above the minimum value
         closest_multiple_100 = -(-range_min // 100) * 100
         clicks_at_x = [x for x in range(closest_multiple_100, range_max + 1, 100)]
@@ -69,7 +66,6 @@ def get_new_password(instructions, starting_point):
 
         if y != old_y:
             if old_y % 100 == 0:
-                print('yes')
                 count_of_zeros += (number_of_clicks_per_step - 1)
             else:
                 count_of_zeros += number_of_clicks_per_step 
@@ -101,10 +97,38 @@ if __name__ == '__main__':
     expected_password = 6
 
     test_y, test_password = get_password_0x434C49434B(test_instructions, starting_point)
-    print(test_y, test_password)
+    assert test_y == expected_y, "dial point wrong"
+    assert test_password == expected_password, "password wrong"
 
     test_y, test_password = get_new_password(test_instructions, starting_point)
-    print(test_y, test_password)
+    assert test_y == expected_y, "dial point wrong in quicker method"
+    assert test_password == expected_password, "password wrong in quicker method"
+
+
+    # actual use
+    starting_point = 50
+    filename = "day-1-dial-input.txt"
+    instructions = parse_input(filename)
+
+
+    y, password = get_new_password(instructions, starting_point)
+    print(f"y: {y}, password: {password}")
+
+    # test speed
+    def solution_1():
+        y, password = get_password_0x434C49434B(instructions, starting_point)
+
+    def solution_2():
+        y, password = get_new_password(instructions, starting_point)
+    
+
+
+    execution_time_1 = timeit.timeit("solution_1", setup="from __main__ import solution_1", number=10000)
+    execution_time_2 = timeit.timeit("solution_2", setup="from __main__ import solution_2", number=10000)
+
+    print(execution_time_1)
+    print(execution_time_2)
+
 
 
 
