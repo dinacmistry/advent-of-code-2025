b59adc24f3bc62e811a08dfda0153d71c749fe22
Dina Mistry <dina.c.mistry@gmail.com>
2025-12-27 22:13:13 -0500

    part 2 solved

--- day-6-trash-compactor.py
+++ day-6-trash-compactor.py
@@ -24,22 +24,31 @@ def parse_inputs(input_string):
 def parse_like_cephalopod(input_string):
 
     split_string = input_string.split("\n")
-
+    
     ncols = len(split_string[0])
-    print(ncols)
-
-    numbers = [ [] for c in range(ncols)]
-    # print(numbers)
+    digits = [ [] for c in range(ncols)]
 
     for i in split_string:
         if ("+" in i) or ("*" in i):
             instructions = list(i)
+        else:
+            line = list(i)
+            line = ['' if l == " " else l for l in line]
+            for n, l in enumerate(line):
+                if l != "":
+                    digits[n].append(l)
 
     has_instructions = [l == "+" or l == "*" for l in instructions]
     has_instructions = np.arange(len(instructions))[has_instructions]
-    print(has_instructions)
-    
-    
+
+    numbers = []
+    for c in range(ncols):
+        col = digits[c]
+        number = sum([int(i) * 10 ** (len(col) - (1 + n)) for n, i in enumerate(col)])
+        numbers.append(number)
+
+    return numbers, instructions, has_instructions
+
 
 
 def cephalopod_homework(numbers, instructions):
@@ -54,11 +63,34 @@ def cephalopod_homework(numbers, instructions):
         solutions.append(answer)
     return sum(solutions)
 
-def fixed_cephalopod_homework(numbers, instructions):
+def fixed_cephalopod_homework(numbers, instructions, has_instructions):
     solutions = []
+    for n in range(len(has_instructions) - 1):
 
-    return sum(solutions)
+        start = has_instructions[n]
+        stop = has_instructions[n+1]
+        instruction = instructions[start]
 
+        answer = [numbers[i] for i in range(start, stop) if numbers[i] != 0]
+        if instruction == "*":
+            answer = np.prod(answer)
+        elif instruction == "+":
+            answer = np.sum(answer)
+        solutions.append(answer)
+
+    # last set of numbers
+    start = has_instructions[-1]
+    stop = len(numbers)
+    instruction = instructions[start]
+
+    answer = [numbers[i] for i in range(start, stop) if numbers[i] != 0]
+    if instruction == "*":
+        answer = np.prod(answer)
+    elif instruction == "+":
+        answer = np.sum(answer)
+    solutions.append(answer)
+
+    return solutions, sum(solutions)
 
 if __name__ == '__main__':
     
@@ -74,7 +106,17 @@ if __name__ == '__main__':
     expected = 4277556
     answer = cephalopod_homework(numbers, instructions)
     assert answer == expected, 'wrong sum'
-    print("Answer", answer)
+    # print("Answer", answer)
+
+    numbers, instructions, has_instructions = parse_like_cephalopod(test)
+    # print(numbers)
+    # print(instructions)
+    # print(has_instructions)
+
+    solutions, sum_of_solutions = fixed_cephalopod_homework(numbers, instructions, has_instructions)
+    expected_sum = 3263827
+    assert sum_of_solutions == expected_sum, "wrong sum"
+
 
 
     filename = "day-6-input.txt"
@@ -84,9 +126,19 @@ if __name__ == '__main__':
 
     numbers, instructions = parse_inputs(contents)
     answer = cephalopod_homework(numbers, instructions)
-    print("Answer", answer)
+    print("Answer to part 1", answer)
+
+    numbers, instructions, has_instructions = parse_like_cephalopod(contents)
+    solutions, sum_of_solutions = fixed_cephalopod_homework(numbers, instructions, has_instructions)
+    # print(solutions)
+    print("Solution to part 2", sum_of_solutions)
 
 
 
-    parse_like_cephalopod(test)
+
+
+
+
+
+
 
