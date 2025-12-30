a4c57f4aac267c793c027e453f2731e68a2d0818
Dina Mistry <dina.c.mistry@gmail.com>
2025-12-29 19:37:59 -0500

    day 8 part 1 solved

--- day-8-playground.py
+++ day-8-playground.py
@@ -14,8 +14,8 @@ def parse_input(input_string):
         elif len(pos) == 0:
             pass
         else:
-            print("error")
-    # positions = np.array(positions)
+            print(i)
+            pass
     return positions
 
 def get_distance_sqrd(p1, p2):
@@ -27,7 +27,43 @@ def find_circuit(junction, circuits):
     for c, circuit in enumerate(circuits):
         if junction in circuit:
             return c
-    else None
+    return None
+
+
+def connect_junctions(junctions, merge_cap, top_n):
+    # pairs of junction ids with their distances
+    all_pairs_with_distances = []
+
+    for i in range(len(junctions)):
+        junction_i = junctions[i]
+        for j in range(i+1, len(junctions)):
+            junction_j = junctions[j]
+            distance = get_distance_sqrd(junction_i, junction_j)
+            all_pairs_with_distances.append([distance, i, j])
+
+    # sort them by distance
+    sorted_pairs_with_distances = copy.deepcopy(all_pairs_with_distances)
+    sorted_pairs_with_distances.sort()
+
+    # put all junctions into their own circuit by id not position
+    circuits = [{i} for i in range(len(junctions))]
+
+    for n in range(merge_cap):
+        distance, i, j = sorted_pairs_with_distances[n]
+        ci = find_circuit(i, circuits)
+        cj = find_circuit(j, circuits)
+
+        # if the circuits aren't the same, let's merge them
+        if ci != cj:
+            circuits[ci] = circuits[ci].union(circuits[cj])
+            # now delete the second set because it got merged
+            del circuits[cj]
+    sorted_circuits = copy.deepcopy(circuits)
+    sorted_circuits.sort(key = lambda circuit: len(circuit), reverse=True)
+
+    top_n_circuit_lengths = [len(sorted_circuits[n]) for n in range(top_n)]
+    return np.prod(top_n_circuit_lengths)
+
 
 
 if __name__ == '__main__':
@@ -36,48 +72,30 @@ if __name__ == '__main__':
     with open(test, 'r') as f:
         contents = f.read()
     f.close()
-    positions = parse_input(contents)
-
-    print(positions)
-    positions = list(positions)
-
-    all_pairs = []
-
-    for i in range(len(positions)):
-        pos_i = positions[i]
-        for j in range(i+1, len(positions)):
-            # print(i, j)
-            pos_j = positions[j]
-            all_pairs.append((pos_i, pos_j))
-
-    sorted_pairs = copy.deepcopy(all_pairs)
-    sorted_pairs.sort(key=lambda pair: get_distance_sqrd(pair[0], pair[1]))
-    # print(sorted_pairs)
-
+    junctions = parse_input(contents)
+    junctions = list(junctions)
 
     merge_cap = 10
+    top_n = 3
 
-    # put all junctions into their own circuit
-    circuits = [set(p) for p in positions]
-
-
-
-    # for n, (i, j) in enumerate(sorted_pairs):
-
-    #     print(n, i, j, get_distance_sqrd(i, j))
-    #     # for chain in chains:
-        #     if (i in chain) or (j in chain):
-        #         chain.add(i)
-        #         chain.add(j)
+    expected = 40
+    answer = connect_junctions(junctions, merge_cap, top_n)
+    print(answer)
+    assert answer == expected, "wrong product"
 
 
+    filename = "day-8-junction-boxes-full.txt"
+    with open(filename, 'r') as f:
+        contents = f.read()
+    f.close()
+    junctions = parse_input(contents)
+    junctions = list(junctions)
+
+    merge_cap = 1000
+    top_n = 3
+
+    answer = connect_junctions(junctions, merge_cap, top_n)
+    print("Product of three largest circuits", answer)
 
 
-        # if n > merge_cap:
-        #     break
 
-        
-
-
-    # print(sorted(positions))
-    # print(np.sort(positions, axis=1))
