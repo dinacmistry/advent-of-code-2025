d3eb6ac06c8a1ad19d75b2df3049448fa728afba
Dina Mistry <dina.c.mistry@gmail.com>
2025-12-29 20:01:42 -0500

    day 8 part 2 solved

--- day-8-playground.py
+++ day-8-playground.py
@@ -41,7 +41,7 @@ def connect_junctions(junctions, merge_cap, top_n):
             distance = get_distance_sqrd(junction_i, junction_j)
             all_pairs_with_distances.append([distance, i, j])
 
-    # sort them by distance
+    # sort pairs by distance
     sorted_pairs_with_distances = copy.deepcopy(all_pairs_with_distances)
     sorted_pairs_with_distances.sort()
 
@@ -64,6 +64,40 @@ def connect_junctions(junctions, merge_cap, top_n):
     top_n_circuit_lengths = [len(sorted_circuits[n]) for n in range(top_n)]
     return np.prod(top_n_circuit_lengths)
 
+def connect_junctions_until_n_circuits(junctions):
+    # pairs of junction ids with their distances
+    all_pairs_with_distances = []
+    for i in range(len(junctions)):
+        junction_i = junctions[i]
+        for j in range(i+1, len(junctions)):
+            junction_j = junctions[j]
+            distance = get_distance_sqrd(junction_i, junction_j)
+            all_pairs_with_distances.append([distance, i, j])
+
+    # sort pairs by distance
+    # reverse order so we pop off the last pair in the list
+    sorted_pairs_with_distances = copy.deepcopy(all_pairs_with_distances)
+    sorted_pairs_with_distances.sort(reverse = True)
+
+    # put all junctions into their own circuit by id not position
+    circuits = [{i} for i in range(len(junctions))]
+
+    # pop off the last value - it's the current pair with the smallest distance
+    while len(circuits) > 1:
+        distance, i, j = sorted_pairs_with_distances.pop()
+        ci = find_circuit(i, circuits)
+        cj = find_circuit(j, circuits)
+
+        # if the circuits aren't the same, let's merge them
+        if ci != cj:
+            circuits[ci] = circuits[ci].union(circuits[cj])
+            # now delete the second set because it got merged
+            del circuits[cj]
+
+    junction_i = junctions[i]
+    junction_j = junctions[j]
+    x_product = junction_i[0] * junction_j[0]
+    return x_product
 
 
 if __name__ == '__main__':
@@ -98,4 +132,5 @@ if __name__ == '__main__':
     print("Product of three largest circuits", answer)
 
 
-
+    x_product = connect_junctions_until_n_circuits(junctions)
+    print("x product of last two junction boxes connected", x_product)
